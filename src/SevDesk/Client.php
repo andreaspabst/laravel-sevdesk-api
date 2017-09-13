880ab62db51030d9677bd325f2fb23b90e06fdf1
Andreas Pabst MacBook Air <a.pabst@weilscoolis.de>
2017-09-13 10:18:30 +0200

    initial commit

---
+++ src/SevDesk/Client.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* File:     Client.php
+* Category: Helper
+* Author:   M. Goldenbaum
+* Created:  19.01.17 22:21
+* Updated:  -
+*
+* Description:
+*  -
+*/
+
+namespace Andreaspabst\SevDesk;
+
+use Illuminate\Support\Facades\Config;
+
+use Andreaspabst\SevDesk\Exceptions\ConnectionFailedException;
+
+class Client {
+
+    
+}
---
+++ src/SevDesk/Exceptions/ConnectionFailedException.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* File:     ConnectionFailedException.php
+* Category: Exception
+* Author:   Andreas Pabst
+* Created:  13.09.17 10:12
+* Updated:  -
+*
+* Description:
+*  -
+*/
+
+namespace Andreaspabst\SevDesk\Exceptions;
+
+use \Exception;
+
+class ConnectionFailedException extends Exception {
+
+}
